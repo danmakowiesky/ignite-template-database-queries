4bd82377e857d35a33a559a45315a36d8f96f7dd
Daniel Makowiesky <daniel.makowiesky@smarketsolutions.com.br>
2021-11-02 10:58:42 -0300

    raw query

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,23 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const games = await this.repository
+    .createQueryBuilder('games')
+    .where('games.title ILIKE :title', {title: `%${param}`}).getMany()
+
+    return games;
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT count(*) FROM games)');  
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const users = await this.repository.createQueryBuilder()
+    .relation(Game, "users")
+    .of(id)
+    .loadMany()
+
+    return users
   }
 }
